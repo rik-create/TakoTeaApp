4f33882ee84e9190e540422fd3e9a8ddf259dcbc
rik-create <reyes.288017@balagtas.sti.edu.ph>
2024-10-28 14:52:09 +0800

    reset

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TakoTea.Dashboard;
 
 namespace TakoTea
 {
@@ -16,7 +17,7 @@ namespace TakoTea
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form1());
         }
     }
 }
